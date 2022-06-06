217e15cfc21b919226cdd3ff15641c69164a7454
Daniel Galvis <d.galvis@bham.ac.uk>
2022-06-06 12:13:42 +0100

    add some new runs

--- GP_smooth.m
+++ GP_smooth.m
@@ -1,5 +1,5 @@
 %load data
-load(fullfile('results_hypercube_attempt_05', 'out_concat.mat'))
+load(fullfile('results_hypercube_attempt_04', 'out_concat.mat'))
 X = assort_concat;
 Y = G_concat;
 Z = pks_concat;
