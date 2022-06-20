5a974f65c12d7dd4a132d4547ea11a29ea24151e
Daniel Galvis <d.galvis@bham.ac.uk>
2022-06-20 11:14:22 +0100

    add results

--- run_concat_hypercube.m
+++ run_concat_hypercube.m
@@ -37,20 +37,40 @@ function run_concat_hypercube(name)
     ord_concat = zeros(config.num_pars, 1);
     ord_p1_concat = zeros(config.num_pars, 1);
     ord_p2_concat = zeros(config.num_pars, 1);
+    fails = [];
+    success = [];
+    
     for i = 1:config.num_pars
-        load(fullfile(dout, ['out_', num2str(i), '.mat']), 'net', 'pvals', 'assort');
-        
-        assort_concat(i) = assort;
-        G_concat(i)      = pvals(1);
-        gconn_concat(i)      = pvals(2);
-        pks_concat(i)    = mean(sum(net.sys.pk_locs));
-        pks_p1_concat(i)    = mean(sum(net.sys.pk_locs(:, logical(net.sys.pops))));
-        pks_p2_concat(i)    = mean(sum(net.sys.pk_locs(:, ~logical(net.sys.pops))));
-        ord_concat(i)    = net.sys.ord;
-        ord_p1_concat(i)    = net.sys.ord_pops(2);
-        ord_p2_concat(i)    = net.sys.ord_pops(1);
+        try
+            load(fullfile(dout, ['out_', num2str(i), '.mat']), 'net', 'pvals', 'assort');
+
+            assort_concat(i) = assort;
+            G_concat(i)      = pvals(1);
+            gconn_concat(i)      = pvals(2);
+            pks_concat(i)    = mean(sum(net.sys.pk_locs));
+            pks_p1_concat(i)    = mean(sum(net.sys.pk_locs(:, logical(net.sys.pops))));
+            pks_p2_concat(i)    = mean(sum(net.sys.pk_locs(:, ~logical(net.sys.pops))));
+            ord_concat(i)    = net.sys.ord;
+            ord_p1_concat(i)    = net.sys.ord_pops(2);
+            ord_p2_concat(i)    = net.sys.ord_pops(1);
+            success = [success, i];
+        catch
+            fails = [fails, i];
+        end
+            
     end
-    clear i  net pvals assort config;
+    
+    assort_concat = assort_concat(success);
+    G_concat = G_concat(success);
+    gconn_concat = gconn_concat(success);
+    pks_concat = pks_concat(success);
+    pks_p1_concat = pks_p1_concat(success);
+    pks_p2_concat = pks_p2_concat(success);
+    ord_concat = ord_concat(success);
+    ord_p1_concat = ord_p1_concat(success);
+    ord_p2_concat = ord_p2_concat(success);
+    
+    clear i net pvals assort config;
     save(fullfile(dout,'out_concat'));
 
 end
--- run_concat_netgen.m
+++ run_concat_netgen.m
@@ -37,20 +37,39 @@ function run_concat_netgen(name)
     ord_concat = zeros(config.num_pars, 1);
     ord_p1_concat = zeros(config.num_pars, 1);
     ord_p2_concat = zeros(config.num_pars, 1);
+    fails = [];
+    success = [];
+    
     for i = 1:config.num_pars
-        load(fullfile(dout, ['out_', num2str(i), '.mat']), 'net', 'pvals', 'assort');
-        
-        assort_concat(i) = assort;
-        G_concat(i)      = pvals(1);
-        gconn_concat(i)      = pvals(2);
-        pks_concat(i)    = mean(sum(net.pk_locs));
-        pks_p1_concat(i)    = mean(sum(net.pk_locs(:, logical(net.pops))));
-        pks_p2_concat(i)    = mean(sum(net.pk_locs(:, ~logical(net.pops))));
-        ord_concat(i)    = net.ord;
-        ord_p1_concat(i)    = net.ord_pops(2);
-        ord_p2_concat(i)    = net.ord_pops(1);
+        try
+            load(fullfile(dout, ['out_', num2str(i), '.mat']), 'net', 'pvals', 'assort');
+
+            assort_concat(i) = assort;
+            G_concat(i)      = pvals(1);
+            gconn_concat(i)      = pvals(2);
+            pks_concat(i)    = mean(sum(net.pk_locs));
+            pks_p1_concat(i)    = mean(sum(net.pk_locs(:, logical(net.pops))));
+            pks_p2_concat(i)    = mean(sum(net.pk_locs(:, ~logical(net.pops))));
+            ord_concat(i)    = net.ord;
+            ord_p1_concat(i)    = net.ord_pops(2);
+            ord_p2_concat(i)    = net.ord_pops(1);
+            success = [success, i];
+        catch
+            fails = [fails, i];
+        end
     end
-    clear i  net pvals assort config;
+    
+    assort_concat = assort_concat(success);
+    G_concat = G_concat(success);
+    gconn_concat = gconn_concat(success);
+    pks_concat = pks_concat(success);
+    pks_p1_concat = pks_p1_concat(success);
+    pks_p2_concat = pks_p2_concat(success);
+    ord_concat = ord_concat(success);
+    ord_p1_concat = ord_p1_concat(success);
+    ord_p2_concat = ord_p2_concat(success);
+    
+    clear i net pvals assort config;
     save(fullfile(dout,'out_concat'));
 
 end
