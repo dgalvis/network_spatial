5659649a7f41a36bd1808aa8c607f9220f1cc7ee
Daniel Galvis <d.galvis@bham.ac.uk>
2022-07-24 19:40:40 +0100

    new runs and results

--- run_preprocess_all.m
+++ run_preprocess_all.m
@@ -3,29 +3,34 @@
 % run_preproc_hypercube('results_netgen_attempt_srk_G_high','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_fn_G_high','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_srk_G_high','pureQuadratic','ardmatern52', 'exact');
-% 
+
+
+% run_preproc_hypercube('results_netgen_attempt_fn_G_high_beta_high','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_netgen_attempt_fn_G_high_beta_low','pureQuadratic','ardmatern52', 'exact');
+
+
 % run_preproc_hypercube('results_netgen_attempt_fn_gconn','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_netgen_attempt_srk_gconn','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_fn_gconn','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_srk_gconn','pureQuadratic','ardmatern52', 'exact');
+% 
+% run_preproc_hypercube('results_netgen_attempt_fn_G_mid','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_netgen_attempt_srk_G_mid','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_hypercube_attempt_fn_G_mid','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_hypercube_attempt_srk_G_mid','pureQuadratic','ardmatern52', 'exact');
+% 
+% run_preproc_hypercube('results_netgen_attempt_fn_G_low','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_netgen_attempt_srk_G_low','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_hypercube_attempt_fn_G_low','pureQuadratic','ardmatern52', 'exact');
+% run_preproc_hypercube('results_hypercube_attempt_srk_G_low','pureQuadratic','ardmatern52', 'exact');
+
 
 %run_preproc_hypercube('results_netgen_attempt_fn_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
 % run_preproc_hypercube('results_netgen_attempt_srk_G_and_gconn','pureQuadratic','ardmatern52');
-run_preproc_hypercube('results_hypercube_attempt_fn_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
+% run_preproc_hypercube('results_hypercube_attempt_fn_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
 % run_preproc_hypercube('results_hypercube_attempt_srk_G_and_gconn','pureQuadratic','ardmatern52');
 
-% I tried some stuff with these, so rerun
-% run_preproc_hypercube('results_netgen_attempt_fn_G_high_beta_high','pureQuadratic','ardmatern52');
-% run_preproc_hypercube('results_netgen_attempt_fn_G_high_beta_low','pureQuadratic','ardmatern52');
 
-%run_preproc_hypercube('results_netgen_attempt_fn_G_mid', 'None','ardsquaredexponential');
-%run_preproc_hypercube('results_netgen_attempt_srk_G_mid','pureQuadratic','ardmatern52');
-%run_preproc_hypercube('results_hypercube_attempt_fn_G_mid','pureQuadratic','ardmatern52');
-%run_preproc_hypercube('results_hypercube_attempt_srk_G_mid','pureQuadratic','ardmatern52');
 
-% run_preproc_hypercube('results_netgen_attempt_fn_G_low');
-% run_preproc_hypercube('results_netgen_attempt_srk_G_low');
-% run_preproc_hypercube('results_hypercube_attempt_fn_G_low');
-% run_preproc_hypercube('results_hypercube_attempt_srk_G_low');
-% 
+
 
