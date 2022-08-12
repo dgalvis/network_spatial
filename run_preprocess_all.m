156fcb2b8c791f1a95bf3b456d88433a972bda7d
Daniel Galvis <d.galvis@bham.ac.uk>
2022-08-12 12:42:44 +0100

    new results

--- run_preprocess_all.m
+++ run_preprocess_all.m
@@ -16,14 +16,14 @@
 % run_preproc_hypercube('results_netgen_attempt_srk_gconn','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_fn_gconn','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_srk_gconn','pureQuadratic','ardmatern52', 'exact');
-% 
+
 % run_preproc_hypercube('results_netgen_attempt_fn_G_mid','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_netgen_attempt_srk_G_mid','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_fn_G_mid','pureQuadratic','ardmatern52', 'exact');
 % run_preproc_hypercube('results_hypercube_attempt_srk_G_mid','pureQuadratic','ardmatern52', 'exact');
 
-run_preproc_hypercube('results_netgen_attempt_srk_G_mid_longT','pureQuadratic','ardmatern52', 'exact');
-run_preproc_hypercube('results_hypercube_attempt_srk_G_mid_longT','pureQuadratic','ardmatern52', 'exact');
+%run_preproc_hypercube('results_netgen_attempt_srk_G_mid_longT','pureQuadratic','ardmatern52', 'exact');
+%run_preproc_hypercube('results_hypercube_attempt_srk_G_mid_longT','pureQuadratic','ardmatern52', 'exact');
 
 % 
 % run_preproc_hypercube('results_netgen_attempt_fn_G_low','pureQuadratic','ardmatern52', 'exact');
@@ -33,11 +33,11 @@ run_preproc_hypercube('results_hypercube_attempt_srk_G_mid_longT','pureQuadratic
 
 
 %run_preproc_hypercube('results_netgen_attempt_fn_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
-run_preproc_hypercube('results_netgen_attempt_srk_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
+%run_preproc_hypercube('results_netgen_attempt_srk_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
 % run_preproc_hypercube('results_hypercube_attempt_fn_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
-run_preproc_hypercube('results_hypercube_attempt_srk_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
+%run_preproc_hypercube('results_hypercube_attempt_srk_G_and_gconn','pureQuadratic','ardmatern52', 'bcd');
 
 %run_preproc_hypercube('results_netgen_attempt_fn_G_and_gconn_p0','pureQuadratic','ardmatern52', 'bcd');
-run_preproc_hypercube('results_netgen_attempt_srk_G_and_gconn_p0','pureQuadratic','ardmatern52', 'bcd');
+%run_preproc_hypercube('results_netgen_attempt_srk_G_and_gconn_p0','pureQuadratic','ardmatern52', 'bcd');
 
 
